0af5e0a044019f03d77acf338a7e13aeeb2d52e1
paul604 <paulorhon@live.fr>
2017-03-06 09:22:48 +0100

    tp1 & serveur py

---
+++ TP Sockets/V1/V1.1/serv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# serveur en python 3
+
+import socket
+
+HOST = ''
+PORT = 50007
+
+s = socket()
+s.bind((HOST, PORT))
+s.listen(7)
+
+conn, addr = s.accept()
+print 'connecte par ', addr
+while 1:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send('serv '+data)
+conn.close()
